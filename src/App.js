57bc3d3e1dc57af480ba32f56d7afeb34c670d31
jacob <jbutler8@terpmail.umd.edu>
2017-12-12 17:34:03 -0800

    update splash font and placement styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
 // UI Components
