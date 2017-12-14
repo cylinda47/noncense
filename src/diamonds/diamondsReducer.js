f86fa84290a93c4424f2c9719060e6625a26b195
jacob <jbutler8@terpmail.umd.edu>
2017-12-13 19:23:45 -0800

    built diamond show page and diamond form

--- src/diamonds/diamondsReducer.js
+++ src/diamonds/diamondsReducer.js
@@ -1,18 +1,23 @@
-import { RECEIVE_ALL_DIAMONDS } from '../diamonds/DiamondsActions';
-
+import {
+  RECEIVE_ALL_DIAMONDS,
+  RECEIVE_DIAMOND
+} from '../diamonds/DiamondsActions';
+import merge from 'lodash/merge';
 
 const initialState = {
     data: null
 }
 
 const diamondsReducer = (state = initialState, action) => {
-    let newState; 
+    let newState;
     switch(action.type){
-       case RECEIVE_ALL_DIAMONDS:
-           return action.diamonds; 
-        default:
-            return state; 
+      case RECEIVE_ALL_DIAMONDS:
+        return action.diamonds;
+      case RECEIVE_DIAMOND:
+        return merge({}, state, {[action.diamond.id]: action.diamond});
+      default:
+        return state;
     }
 }
 
-export default diamondsReducer; 
+export default diamondsReducer;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { UserIsAuthenticated, UserIsNotAuthenticated } from './util/wrappers.js'
-import DiamondsIndexContainer from './diamonds/components/DiamondsIndexContainer'; 
+import DiamondsIndexContainer from './diamonds/components/DiamondsIndexContainer';
 import getWeb3 from './util/web3/getWeb3'
 
 // Layouts
 import App from './App'
 import Home from './layouts/home/Home'
 import SignUp from './user/layouts/signup/SignUp'
-import DiamondFormContainer from './user/ui/diamondform/DiamondFormContainer'
+import DiamondShow from './diamonds/diamondShow/DiamondShowContainer'
+import DiamondFormContainer from './diamonds/diamondForm/DiamondFormContainer'
 
 // Redux Store
 import store from './store'
@@ -28,16 +29,15 @@ getWeb3
   console.log('Error in web3 initialization.')
 })
 
-
-
 ReactDOM.render((
     <Provider store={store}>
       <Router history={history}>
         <Route path="/" component={App}>
           <IndexRoute component={Home} />
           <Route path="signup" component={UserIsNotAuthenticated(SignUp)} />
-          <Route path="diamonds" component={UserIsAuthenticated(DiamondsIndexContainer)} />
+          <Route path="diamonds/:diamondId" component={UserIsNotAuthenticated(DiamondShow)} />
           <Route path="new" component={UserIsAuthenticated(DiamondFormContainer)} />
+          <Route path="diamonds" component={UserIsAuthenticated(DiamondsIndexContainer)} />
         </Route>
       </Router>
     </Provider>
