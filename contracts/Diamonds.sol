2dea01d7d65520451e3fa721cf0ae0c458ba5de9
William Meng <willmeng@gmail.com>
2017-12-13 19:16:44 -0800

    Smart contract now uses returns all arrays in getAllDiamonds

--- contracts/Diamonds.sol
+++ contracts/Diamonds.sol
@@ -1,57 +1,77 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
 contract Diamonds {
-    struct Diamond {
-        uint id;
-        uint price; // in ether (should convert to Wei)
-        address ownerAddr;
-    }
-    
-    Diamond[] public allDiamonds; // array of Diamond structs
-    mapping(uint => Diamond) public diamonds; 
-    address owner;
-    
-    event DiamondLogger(uint, uint, address);
+   // separate arrays for each diamond field. Index is the diamondId
+    uint[] diamondPrices;
+    address[] diamondOwners;
+    string[] diamondNames;
+    string public diamondNamesString;
+
+    address private owner;
     
     function Diamonds() public {
-        owner = msg.sender;
-    }
-
-    function getDiamondId() public constant returns (uint diamondId) {
-        return allDiamonds.length;
+        // constructor function
+        owner = msg.sender; // contract owner
     }
     
-    function createDiamond(uint price) public {
-        // uint diamondId = allDiamonds.length;
-        allDiamonds.push(Diamond(0, price, msg.sender));
-        diamonds[0] = Diamond(0, price, msg.sender); 
-        
-        // DiamondLogger(allDiamonds[diamondId].id, allDiamonds[diamondId].price, allDiamonds[diamondId].ownerAddr);
+    function createDiamond(string name, uint price) public returns (uint) {
+        diamondNames.push(name);
+        diamondNamesString = concat(diamondNamesString, name, "|");
+        diamondPrices.push(price);
+        diamondOwners.push(msg.sender);
+
+        return diamondPrices.length - 1; // return diamondId
     }
     
-    function buy(uint diamondId) payable public {
-        //find diamond (diamondId should be index of diamond in allDiamonds)
-        if (allDiamonds.length > diamondId && 
-            msg.value >= allDiamonds[diamondId].price * 1e18) {
-            allDiamonds[diamondId].ownerAddr.transfer(allDiamonds[diamondId].price * 1e18);
-            allDiamonds[diamondId].ownerAddr = msg.sender;
-        }
+    function buy(uint diamondId) payable public returns (uint) {
+        // diamondId is index of diamond in the arrays
+        require(diamondPrices.length > diamondId && 
+            msg.value >= diamondPrices[diamondId] * 1e18);
+
+        diamondOwners[diamondId].transfer(diamondPrices[diamondId] * 1e18);
+        diamondOwners[diamondId] = msg.sender; // change owner
+
+        return diamondId;
     }
 
-    function getDiamond(uint diamondId) public returns(uint, uint, address) {
-        Diamond d = diamonds[diamondId];
-        
-        // break the struct's members out into a tuple
-        // in the same order that they appear in the struct
-        return (d.id, d.price, d.ownerAddr);
-    }
-
-    function diamondsTot() public returns (address, uint) {
-        return (msg.sender, allDiamonds.length); 
+    function getAllDiamonds()
+        view
+        public 
+        returns (string, uint[], address[]) 
+    {
+        return (diamondNamesString, diamondPrices, diamondOwners);
     }
     
     function kill() public {
-        if (msg.sender == owner) 
-          selfdestruct(owner);
+        // destroy contract and send all remaining funds to contract owner's address
+        require (msg.sender == owner);
+        selfdestruct(owner);
+    }
+    
+    function concat(string _base, string _value, string _separator) 
+        pure 
+        internal 
+        returns (string) 
+        {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+        
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length + 1);
+        bytes memory _newValue = bytes(_tmpValue);
+        
+        uint i;
+        uint j;
+        
+        for (i=0; i < _baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
+        }
+        
+        _newValue[j++] = bytes(_separator)[0];
+        
+        for (i=0; i < _valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i];
+        }
+        
+        return string(_newValue);
     }
 }
