66f87d1482389102c4510dd875f03d5b0d9edd2f
Linda Chan <cylinda0407@gmail.com>
2017-12-14 19:17:59 -0800

    Resolve conflict

--- src/diamonds/diamondsIndex/DiamondsIndex.jsx
+++ src/diamonds/diamondsIndex/DiamondsIndex.jsx
@@ -18,33 +18,31 @@ class DiamondsIndex extends React.Component {
         this.setState({diamonds: newProps.diamonds})
     }
 
-    render(){
-        let diamonds; 
-        if(this.state.diamonds[0]){ 
-            
-           
-            diamonds = Object.keys(this.state.diamonds).map(diamondId => {
-
-                return(
+    render(){       
+        console.log(this.state);
+        let diamonds;
+        console.log(this.state.diamonds[0]);
+        if (this.state.diamonds[0]) {
+            console.log("im here!!");
+            diamonds = Object.keys(this.state.diamonds).map(diamondId => (
                 <div className="diamonds-item">
-                    <br/><br/><br/>
-                    <img className="diamond-img"src="https://www.whiteflash.com/images/rd/articles/A-CUT-ABOVE-Diamond-sm.jpg"
-                    width="40" height="40" alt=""/>
+                    <button className="diamond-buy">Buy</button>
                     <ul className="diamonds-properites-list">
-                        <Link to={`diamonds/${diamondId}`}>{this.state.diamonds[diamondId].name}</Link>
-                        <li>{this.state.diamonds[diamondId].price}</li>
-                        <li>{this.state.diamonds[diamondId].address}</li>
+                        <li><img src="https://bnsec.bluenile.com/bluenile/is/image/bluenile/-graudated-milgrain-diamond-engagement-ring-14k-gold-/53700_main?$phab_detailmain$" /></li>
+                        <li className="diamond-name"><Link to={`diamonds/${diamondId}`}>{this.state.diamonds[diamondId].name}</Link></li>
+                        <li className="diamond-price">USD $ {this.state.diamonds[diamondId].price}.00</li>
+                        {/* <li>{this.state.diamonds[diamondId].address}</li> */}
                     </ul>
                 </div>
-                )
-            });
+            ));
         }else{
             diamonds = <li>Diamonds</li>
         }
 
         return (
             <div className="diamonds-container">
-                {diamonds}
+                <h1>Search for Diamonds</h1>
+                <div className="diamonds-item-container">{diamonds}</div>
             </div>
         )
 
