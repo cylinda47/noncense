9b9bf9083ce1ed96a98a4f5e78dcedb306f2bb03
jacob <jbutler8@terpmail.umd.edu>
2017-12-15 11:44:16 -0800

    commit

--- src/diamonds/diamondsIndex/DiamondsIndex.jsx
+++ src/diamonds/diamondsIndex/DiamondsIndex.jsx
@@ -18,25 +18,9 @@ class DiamondsIndex extends React.Component {
         this.setState({diamonds: newProps.diamonds})
     }
 
-    render(){       
+    render(){
         console.log(this.state);
         let diamonds;
-<<<<<<< HEAD
-        if(this.state.diamonds[0]){
-
-
-            diamonds = Object.keys(this.state.diamonds).map(diamondId => {
-
-                return(
-                <div className="diamonds-item">
-                    <br/><br/><br/>
-                    <img className="diamond-img"src="https://www.whiteflash.com/images/rd/articles/A-CUT-ABOVE-Diamond-sm.jpg"
-                    width="40" height="40" alt=""/>
-                  <ul className="diamonds-properties-list">
-                        <Link to={`diamonds/${diamondId}`}>{this.state.diamonds[diamondId].name}</Link>
-                        <li>{this.state.diamonds[diamondId].price}</li>
-                        <li>{this.state.diamonds[diamondId].address}</li>
-=======
         console.log(this.state.diamonds[0]);
         if (this.state.diamonds[0]) {
             console.log("im here!!");
@@ -47,8 +31,7 @@ class DiamondsIndex extends React.Component {
                         <li><img src="https://bnsec.bluenile.com/bluenile/is/image/bluenile/-graudated-milgrain-diamond-engagement-ring-14k-gold-/53700_main?$phab_detailmain$" /></li>
                         <li className="diamond-name"><Link to={`diamonds/${diamondId}`}>{this.state.diamonds[diamondId].name}</Link></li>
                         <li className="diamond-price">USD $ {this.state.diamonds[diamondId].price}.00</li>
-                        {/* <li>{this.state.diamonds[diamondId].address}</li> */}
->>>>>>> d84864fea5b050d021d600784075773005e125f8
+
                     </ul>
                 </div>
             ));
